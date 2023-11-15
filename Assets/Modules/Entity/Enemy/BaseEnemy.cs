ec4918a12338d910368f6352f9f27f0a094807cd
mintti <mintti97@gmail.com>
2023-11-15 19:44:49 +0900

    [Feat] 적 패턴 설정 후, 패턴 타입에 따라 사전 행동 할 수 있도록 설정

--- Assets/Modules/Entity/Enemy/BaseEnemy.cs
+++ Assets/Modules/Entity/Enemy/BaseEnemy.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Cardinals.Board;
 using Cardinals.Enemy;
 using Cardinals.Enums;
 using UnityEngine;
@@ -47,31 +49,51 @@ namespace Cardinals
                             int maxHp   ) : base(maxHp)
         {
             Name = name;
+            UpdatePatternEvent += ExecutePreActionByPattern;
         }
 
-        
+        /// <summary>
+        /// 범위 공격 시, 공격할 타일들을 저장하는 리스트 
+        /// </summary>
+        protected List<Tile> AreaAttackTiles { get; } = new();
+
         public override void StartTurn()
         {
             base.StartTurn();
+
             UpdatePatternEvent?.Invoke(CurPattern);
+
+        }
+
+        private void ExecutePreActionByPattern(Pattern pattern)
+        {
+            switch (pattern.Type)
+            {
+                case EnemyActionType.AreaAttack :
+                    AreaAttackTiles.Clear();
+                    pattern.PreAction(); // [TODO] Action을 통해서 공격 범위를 설정함
+                    // [TODO] Board에서 범위 공격 범위 표시 필요
+                    break;
+            }
         }
         
         public override void OnTurn()
         {
             Pattern curPat = CurPattern;
+            int value = curPat.Value ?? 0;
+            
             Debug.Log($"적({Name}): {curPat.Type.ToString()} 수행");
             switch (curPat.Type)
             {
                 case EnemyActionType.Attack :
-                    
+                    GameManager.I.TempPlayer.Hit(value);
                     break;
                 case EnemyActionType.Defense :
-                    
+                    GameManager.I.TempPlayer.DefenseCount += value;
                     break;
                 case EnemyActionType.AreaAttack :
-                    
-                    break;
-                default:
+                case EnemyActionType.Magic :
+                case EnemyActionType.Buff :
                     curPat.Action();
                     break;
             }
