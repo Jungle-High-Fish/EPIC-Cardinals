7cf68bca8961ed19ff9dabb9720d76d95afeb52e
Yu Taehwan <ytw0113@hanyang.ac.kr>
2023-11-24 00:59:39 +0900

    feat: 타일 저주 타입 추가

--- Assets/Modules/Stage/Board/Tile/TileCurse/TileCurseData.cs
+++ Assets/Modules/Stage/Board/Tile/TileCurse/TileCurseData.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Cardinals.Enums;
+
 namespace Cardinals.Board {
 
 	public class TileCurseData {
 		public int TargetTurn => _targetTurn;
 		public Action Action => _curseAction;
 
+		private TileCurseType _curseType;
 		private int _targetTurn;
 		private Action _curseAction;
 
-		public TileCurseData(int targetTurn, Action curseAction) {
+		public TileCurseData(TileCurseType _curseType, int targetTurn, Action curseAction) {
 			_targetTurn = targetTurn;
 			_curseAction = curseAction;
 		}
