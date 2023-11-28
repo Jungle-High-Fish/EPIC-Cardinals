71436a35fc3f2586d274e51b9332a34cbf1b194c
Planete <ytw0113@hanyang.ac.kr>
2023-11-28 20:20:59 +0900

    [Fix] 느낌표 버그 수정

--- Assets/Modules/Player/UIPlayerInfo.cs
+++ Assets/Modules/Player/UIPlayerInfo.cs
@@ -88,6 +88,10 @@ namespace Cardinals
         }
 
         private void ShowAlertImage() {
+            if (_isPanelOpen) {
+                return;
+            }
+
             _alertTr.Get(gameObject).gameObject.SetActive(true);
             _alertTr.Get(gameObject).transform.localScale = Vector3.one * 1.5f;
             _alertTr.Get(gameObject).transform
