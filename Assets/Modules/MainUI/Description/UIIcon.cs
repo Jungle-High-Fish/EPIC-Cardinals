e9100daf6220f22b9d4f562f58966cc17657467f
mintti <mintti97@gmail.com>
2023-12-04 16:08:24 +0900

    [Feat] 축복 활성화 이펙트 추가

--- Assets/Modules/MainUI/Description/UIIcon.cs
+++ Assets/Modules/MainUI/Description/UIIcon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Util;
 using DG.Tweening;
+using Sirenix.OdinInspector;
 
 namespace Cardinals.UI
 {
@@ -18,6 +19,8 @@ namespace Cardinals.UI
         ComponentGetter<Image> _icon
             = new ComponentGetter<Image>(TypeOfGetter.ChildByName, "Icon");
 
+        private Color _backupColor;
+        private bool _isAnimating;
         public void Init()
         {
             
@@ -29,6 +32,7 @@ namespace Cardinals.UI
             transform.DOScale(Vector3.one, 0.4f).SetEase(Ease.OutBack);
 
             _background.Get(gameObject).color = innerColor ?? Constants.Common.Colors.CardinalsWhite;
+            _backupColor = _background.Get(gameObject).color;
             _icon.Get(gameObject).sprite = sprite;
 
             if (sprite == null) {
@@ -38,6 +42,33 @@ namespace Cardinals.UI
             }
 
             GetComponent<DescriptionConnector>().Init();
+        }
+
+        public void EffectDefault()
+        {
+            
+        }
+
+        [Button]
+        public void EffectBless()
+        {
+            if (!_isAnimating)
+            {
+                _isAnimating = true;
+                float duration = 0.5f;
+
+                _icon.Get(gameObject).DOBlendableColor(_backupColor, duration);
+                transform.DOPunchScale(Vector3.one, duration, 1, 0);
+                _background.Get(gameObject).DOColor(Color.white, duration);
+                _background.Get(gameObject).DOFade(.5f, duration)
+                    .OnComplete(() =>
+                    {
+                        _background.Get(gameObject).color = _backupColor;
+                        _icon.Get(gameObject).color = Color.white;
+                        transform.localScale = Vector3.one; 
+                        _isAnimating = false;
+                    });
+            }
         }
     }
 }
