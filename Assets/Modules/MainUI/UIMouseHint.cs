d7044e686cf5a2bdfdc04ab7b54d68f1c628c2e0
Planete <ytw0113@hanyang.ac.kr>
2023-12-05 00:23:56 +0900

    [Feat] 마우스 우클릭 아이콘 추가

---
+++ Assets/Modules/MainUI/UIMouseHint.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cardinals;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Util;
+
+public class UIMouseHint : MonoBehaviour
+{      
+    [SerializeField] private Sprite sprite1;
+    [SerializeField] private Sprite sprite2;
+
+    private ComponentGetter<Image> _image 
+        = new ComponentGetter<Image>(TypeOfGetter.This);
+
+    private IEnumerator _changeSprite;
+
+    public void Show() {
+        gameObject.SetActive(true);
+        _changeSprite = ChangeSprite();
+        StartCoroutine(_changeSprite);
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+        StopCoroutine(_changeSprite);
+    }
+
+    private void Update() {
+        var canvasRectTransform = GameManager.I.UI.MainUICanvas.transform as RectTransform;
+        transform.position = Input.mousePosition + new Vector3(0.5f, 0.5f);
+    }
+
+    private IEnumerator ChangeSprite() {
+        while (true) {
+            _image.Get(gameObject).sprite = sprite1;
+            yield return new WaitForSeconds(0.5f);
+            _image.Get(gameObject).sprite = sprite2;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
