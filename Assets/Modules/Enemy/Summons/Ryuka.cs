50721b95dd4be052145014163f4cf9e2c72ecce5
Mintti <mintti97@gmail.com>
2023-12-11 16:14:34 +0900

    [Feat] 류카 소환수 구현

--- Assets/Modules/Enemy/Summons/Ryuka.cs
+++ Assets/Modules/Enemy/Summons/Ryuka.cs
@@ -8,36 +8,47 @@ namespace Cardinals.Enemy.Summon
 {
     public class Ryuka : BaseEnemySummon
     {
-        public void Init()
+        [Header("Data")]
+        [SerializeField] private int _moveCount;
+        [SerializeField] private float _moveDuration;
+
+        public void Init(Tile tile)
         {
+            base.Init();
+            GameManager.I.Stage.Summons.Add(this);
             
+            _onTile = tile;
+            transform.position = GetPosition(_onTile.gameObject.transform.position);
         }
-        //Vector3 GetPosition(Vector3 vector3) => vector3 + new Vector3(0, _maxY, 0);
+
+        Vector3 GetPosition(Vector3 vector) => vector + new Vector3(0, 1, 0);
         
         public override IEnumerator OnTurn()
         {
-            // var playerTile = GameManager.I.Player.OnTile; 
-            // Tile tile = _onTile;
-            //
-            // for (int i = 0; i < _moveCount; i++)
-            // {
-            //     tile = tile.Next;
-            //     transform.DOMove(GetPosition(tile.transform.position), _moveDuration);
-            //     yield return new WaitForSeconds(_moveDuration);
-            //
-            //     if (playerTile == tile) // 이동했는데 플레이어 타일
-            //     {
-            //         Execute();
-            //         break;
-            //     }
-            // }
-            //
-            // SetTile(tile);
+            TouchedPlayer();
+            
+            var playerTile = GameManager.I.Player.OnTile; 
+            Tile tile = _onTile;
+            
+            for (int i = 0; i < _moveCount; i++)
+            {
+                tile = tile.Next;
+                transform.DOMove(GetPosition(tile.transform.position), _moveDuration);
+                yield return new WaitForSeconds(_moveDuration);
+            
+                if (playerTile == tile) // 이동했는데 플레이어 타일
+                {
+                    Execute();
+                    break;
+                }
+            }
+            
+            SetTile(tile);
             
             yield return null;
         }
 
-        void Execute()
+        void TouchedPlayer()
         {
             var enemy = GameManager.I.CurrentEnemies.FirstOrDefault();
 
@@ -45,6 +56,20 @@ namespace Cardinals.Enemy.Summon
             {
                 enemy.Heal(2);
             }
+        }
+
+        public override void Execute()
+        {
+            transform
+                .DOShakePosition(.5f, .5f)
+                .OnComplete(() =>
+                {
+                    Delete();
+                });
+        }
+        public void OnDestroy()
+        {
+            GameManager.I.Stage.Summons.Remove(this);
         }
     }
 }
