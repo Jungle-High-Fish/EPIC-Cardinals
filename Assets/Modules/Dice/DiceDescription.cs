c0c70375ae5e3b01102e3c622d3270b766c59b82
김은서 <a55208806@gmail.com>
2023-12-12 12:38:57 +0900

    [Feat] 주사위 Description 리소스도 교체했다굴~
    굴

--- Assets/Modules/Dice/DiceDescription.cs
+++ Assets/Modules/Dice/DiceDescription.cs
@@ -18,21 +18,10 @@ namespace Cardinals
             {
                 GameObject surface = Instantiate(ResourceLoader.LoadPrefab(Constants.FilePath.Resources.Prefabs_UI_DiceSurface),_diceDescription.transform);
                 surface.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = numbers[i].ToString();
-                switch (type)
-                {
-                    case DiceType.Normal:
-                        surface.GetComponent<Image>().color = new Color(0.8f, 0.8f, 0.8f);
-                        break;
-                    case DiceType.Fire:
-                        surface.GetComponent<Image>().color = new Color(0.9245283f, 0.51689120f, 0.4404593f);
-                        break;
-                    case DiceType.Water:
-                        surface.GetComponent<Image>().color = new Color(0.4332948f, 0.6019194f, 0.8584906f);
-                        break;
-                    case DiceType.Earth:
-                        surface.GetComponent<Image>().color = new Color(0.6320754f, 0.5570136f, 0.5277234f);
-                        break;
-                }
+
+                string path = "Dice/Dice_" + type.ToString() + "_" + numbers[i].ToString();
+                surface.GetComponent<Image>().sprite = ResourceLoader.LoadSprite(path);
+
             }
         }
 
