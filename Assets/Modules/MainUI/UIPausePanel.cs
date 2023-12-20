370a790acd2668d74c7864a22112af0200d70fb4
Planete <ytw0113@hanyang.ac.kr>
2023-12-20 19:51:07 +0900

    [Feat] 옵션창 구현 중

---
+++ Assets/Modules/MainUI/UIPausePanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cardinals.UI {
+    public class UIStagePanel : MonoBehaviour {
+        public void Init() {
+            
+        }
+    }
+}
